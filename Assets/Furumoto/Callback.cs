9864801c253997306337890f0f769acf23ca0321
furumoto-yasutaka <ika3goma3@gmail.com>
2023-10-28 16:43:43 +0900

    カメラ追加

--- Assets/Furumoto/Callback.cs
+++ Assets/Furumoto/Callback.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Callback : MonoBehaviour
 {
     [SerializeField]
     private Player m_player;
+    [SerializeField]
+    private CinemachineVirtualCamera m_camera;
 
     public void JumpCallback()
     {
@@ -15,5 +18,6 @@ public class Callback : MonoBehaviour
     public void FinishCallback()
     {
         m_player.ChangeEndState();
+        m_camera.Priority = 100;
     }
 }
